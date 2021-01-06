c25257a6357d849b9780c5a156867a4488ea2249
Jeremie <jeremieflahaut@gmail.com>
2021-01-06 14:50:47 +0100

    move public method to interface

--- src/GrantType/GrandTypeInterface.php
+++ src/GrantType/GrandTypeInterface.php
@@ -3,10 +3,19 @@
 namespace JFlahaut\GuzzleOauth2Client\GrantType;
 
 
+use JFlahaut\GuzzleOauth2Client\AccessToken;
+
 interface GrandTypeInterface
 {
+    /**
+     * @return string
+     */
+    public function getGrandType(): string;
 
-
+    /**
+     * @return AccessToken|null
+     */
+    public function getToken(): ?AccessToken;
 
 
 }
